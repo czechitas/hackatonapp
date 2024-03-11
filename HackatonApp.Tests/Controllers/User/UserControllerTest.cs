994b31e4354e176f761f7c7011c1e9c6a654403d
Lukáš Műller <hypous2@gmail.com>
2024-03-11 16:25:30 +0100

    Add styles add tests

---
+++ HackatonApp.Tests/Controllers/User/UserControllerTest.cs
@@ -0,0 +1,16 @@
+using HackatonApp.Controllers.User;
+
+namespace HackatonApp.Tests.Controllers.User
+{
+    [TestFixture]
+    [TestOf(typeof(UserController))]
+    public class UserControllerTest
+    {
+
+        [Test]
+        public void METHOD()
+        {
+            
+        }
+    }
+}
